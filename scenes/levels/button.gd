602f9c8800fd0d0009c5cdb9cb90887f641255b2
the-outer-blue <the.outer.blue@gmail.com>
2025-04-25 04:21:45 +0100

    final_panel

--- scenes/levels/button.gd
+++ scenes/levels/button.gd
@@ -1,11 +1,12 @@
 extends Button
-
-
+@onready var info=$"../Sprite2D6"
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _onpressed():
-	
+func _on_button_down() -> void:
+	info.visible=true
+	print("pressed")
+
+func _on_button_up() -> void:
+	info.visible=false
+	print("unpressed")
